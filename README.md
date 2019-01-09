ce6411a8906c38b9c27faaae6e94f6492862647b
anotherso1a <1181581742@qq.com>
2019-01-09 19:31:14 +0800

    first push

---
+++ README.md
@@ -0,0 +1,6 @@
+# AIjoke
+
+## start
+
+>1.copy the index.js's content into Console
+>2.run chatRobot(str)//like chatRobot('我帅吗?')
---
+++ index.js
@@ -0,0 +1,3 @@
+function chatRobot(question){
+	return Array.prototype.reduce.call(question,(c,v)=>c+(v=='我'?'你':v=='你'?'我':v),'').replace(/\?$|吗\?$|没\?$/,'!')
+}
